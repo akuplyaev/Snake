53066ec0fa89fd65c45b1aa9549c70bace738264
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2016-05-16 08:29:08 +0300

    Begin

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake {
+    class Point {
+        int x;
+        int y;
+        char ch;
+      public  Point(){}
+       public Point(int _x, int _y, char _ch) {
+            x = _x;
+            y = _y;
+            ch = _ch;
+        }
+
+        public void Drow() {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
+        }
+    }
+    
+}
