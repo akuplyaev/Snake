a5df137f763a52a5eaed8b015f42b91f66ee51a9
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2016-05-17 15:36:36 +0300

    fix

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,21 +6,21 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake {
-    class Program {
-        static void Main(string[] args) {
-            Console.SetBufferSize(80, 25);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-            Point p = new Point(3, 4, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();       
-            FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
+	class Program {
+		static void Main(string[] args) {
+			Console.SetBufferSize(80, 25);
+			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+			upLine.Draw();
+			downLine.Draw();
+			leftLine.Draw();
+			rightLine.Draw();
+			Point p = new Point(3, 4, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();       
+			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
@@ -41,11 +41,11 @@ namespace Snake {
 				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HadleKey(key.Key);
+					snake.HadleKey(key.Key);
 				}
 			}
-            
-            
-        }
-    }
+			
+			
+		}
+	}
 }
